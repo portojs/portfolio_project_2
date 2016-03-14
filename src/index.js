27e95575ff6c1337c6993f8ef706ae8d3e7bf01d
peter <peter.k.nets@gmail.com>
2016-03-14 12:50:54 +0200

    Add temp navbar and footer components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import NavBar from './components/nav_bar';
+import Footer from './components/footer';
+
 class App extends Component {
   render() {
     return (
       <div>
-        App
+        <NavBar />
+        Body
+        <Footer />
       </div>
     );
   }
