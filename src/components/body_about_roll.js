f41b8c153c0ff1d09332774c5a086e31c714123c
peter <peter.k.nets@gmail.com>
2016-03-18 11:25:47 +0200

    Add article texts

--- src/components/body_about_roll.js
+++ src/components/body_about_roll.js
@@ -10,7 +10,14 @@ const BodyAboutRoll = () => {
         <div className="col-md-3 pic">
         </div>
         <div className="col-md-3">
-          <h4>Wall Street Journal</h4>
+          <h3>Wall Street Journal</h3>
+          <p>
+            Consequat molestie parturient adipiscing ad scelerisque
+            scelerisque tincidunt rutrum himenaeos hendrerit nunc
+            vestibulum consectetur tempus ullamcorper tincidunt
+            vestibulum vivamus.
+          </p>
+          <h4 className="pull-right">1999</h4>
         </div>
         <div className="col-md-3 pic">
         </div>
@@ -19,12 +26,26 @@ const BodyAboutRoll = () => {
         <div className="col-md-3 pic">
         </div>
         <div className="col-md-3">
-          <h4>Vanity</h4>
+          <h3>Vanity</h3>
+          <p>
+            Consequat molestie parturient adipiscing ad scelerisque
+            scelerisque tincidunt rutrum himenaeos hendrerit nunc
+            vestibulum consectetur tempus ullamcorper tincidunt
+            vestibulum vivamus.
+          </p>
+          <h4 className="pull-right">2012</h4>
         </div>
         <div className="col-md-3 pic">
         </div>
         <div className="col-md-3">
-          <h4>New York Times</h4>
+          <h3>New York Times</h3>
+          <p>
+            Consequat molestie parturient adipiscing ad scelerisque
+            scelerisque tincidunt rutrum himenaeos hendrerit nunc
+            vestibulum consectetur tempus ullamcorper tincidunt
+            vestibulum vivamus.
+          </p>
+          <h4 className="pull-right">2014</h4>
         </div>
       </div>
     </div>
