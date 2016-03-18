928dff5c453d17fdde943806edb8b5f28cd3797d
peter <peter.k.nets@gmail.com>
2016-03-18 10:01:19 +0200

    Add some footer content

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,26 @@ import React from 'react';
 const Footer = (props) => {
   return (
     <footer className="container footer">
-      Footer
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 col-md-offset-3">
+            <ul>
+              <li>Legal Notice</li>
+              <li>Terms & Conditions</li>
+              <li>Disclaimer</li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <ul>
+              <li>Contacts</li>
+              <li>About us</li>
+              <li>Shop</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <p className="pull-left">&copy; All Rights Reserved. Peter Krevenets, 2016.</p>
+      <p className="pull-right">Powered by Insidious Mind</p>
     </footer>
   );
 }
