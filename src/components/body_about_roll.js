d529d09629511b2cda11a666b98115a5c8a42b73
peter <peter.k.nets@gmail.com>
2016-03-18 10:42:18 +0200

    Set basic grid for body-about-roll

---
+++ src/components/body_about_roll.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const BodyAboutRoll = () => {
+  return (
+    <div className="body-about-roll">
+      <div className="row">
+        <div className="col-md-3">
+          <h1 className="lead-text">Press</h1>
+        </div>
+        <div className="col-md-3 pic">
+        </div>
+        <div className="col-md-3">
+          <h4>Wall Street Journal</h4>
+        </div>
+        <div className="col-md-3 pic">
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-3 pic">
+        </div>
+        <div className="col-md-3">
+          <h4>Vanity</h4>
+        </div>
+        <div className="col-md-3 pic">
+        </div>
+        <div className="col-md-3">
+          <h4>New York Times</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BodyAboutRoll;
