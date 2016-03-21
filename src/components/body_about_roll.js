67275d87230ee3358f111e98b9c6fadf3b64bcfd
peter <peter.k.nets@gmail.com>
2016-03-21 09:55:28 +0200

    Fix - make about-roll text-only

--- src/components/body_about_roll.js
+++ src/components/body_about_roll.js
@@ -3,29 +3,91 @@ import React from 'react';
 const BodyAboutRoll = () => {
   return (
     <div className="body-about-roll">
+
       <div className="row">
-        <div className="col-md-3 body-text">
+        <div className="col-md-3 body-heading">
           <h1 className="lead-text">Press</h1>
         </div>
-        <div className="col-md-3 pic">
-          <img src="src/img/keiko1.jpg"/>
-        </div>
-        <div className="col-md-3 body-text">
-          <div className="body-about-article">
-            <h3>Wall Street Journal</h3>
-            <p>
-              Consequat molestie parturient adipiscing ad scelerisque
-              scelerisque tincidunt rutrum himenaeos hendrerit nunc
-              vestibulum consectetur tempus ullamcorper tincidunt
-              vestibulum vivamus.
-            </p>
-            <h4 className="pull-right">1999</h4>
+        <div className="col-md-9">
+          <div className="row">
+            <div className="col-md-4 body-text">
+              <div className="body-about-article">
+                <h3>Los Angeles Outrider</h3>
+                <p>
+                  Consequat molestie parturient adipiscing ad scelerisque
+                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
+                  vestibulum consectetur tempus ullamcorper tincidunt
+                  vestibulum vivamus.
+                </p>
+                <h4 className="pull-right">1994</h4>
+              </div>
+            </div>
+            <div className="col-md-4 body-text">
+              <div className="body-about-article">
+                <h3>Wall Street Journal</h3>
+                <p>
+                  Consequat molestie parturient adipiscing ad scelerisque
+                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
+                  vestibulum consectetur tempus ullamcorper tincidunt
+                  vestibulum vivamus.
+                </p>
+                <h4 className="pull-right">1999</h4>
+              </div>
+            </div>
+            <div className="col-md-4 body-text">
+              <div className="body-about-article">
+                <h3>Vanity</h3>
+                <p>
+                  Consequat molestie parturient adipiscing ad scelerisque
+                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
+                  vestibulum consectetur tempus ullamcorper tincidunt
+                  vestibulum vivamus.
+                </p>
+                <h4 className="pull-right">2012</h4>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4 body-text">
+              <div className="body-about-article">
+                <h3>New York Times</h3>
+                <p>
+                  Consequat molestie parturient adipiscing ad scelerisque
+                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
+                  vestibulum consectetur tempus ullamcorper tincidunt
+                  vestibulum vivamus.
+                </p>
+                <h4 className="pull-right">2014</h4>
+              </div>
+            </div>
+            <div className="col-md-4 body-text">
+              <div className="body-about-article">
+                <h3>Esquire</h3>
+                <p>
+                  Consequat molestie parturient adipiscing ad scelerisque
+                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
+                  vestibulum consectetur tempus ullamcorper tincidunt
+                  vestibulum vivamus.
+                </p>
+                <h4 className="pull-right">2014</h4>
+              </div>
+            </div>
+            <div className="col-md-4 body-text">
+              <div className="body-about-article">
+                <h3>Empire</h3>
+                <p>
+                  Consequat molestie parturient adipiscing ad scelerisque
+                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
+                  vestibulum consectetur tempus ullamcorper tincidunt
+                  vestibulum vivamus.
+                </p>
+                <h4 className="pull-right">2016</h4>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="col-md-3 pic">
-          <img src="src/img/keiko2.jpg"/>
-        </div>
       </div>
+
     </div>
   );
 }
