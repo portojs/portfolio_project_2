a6cd88588dcc894ad0759f424d9ba1a057b31aa4
peter <peter.k.nets@gmail.com>
2016-03-21 12:41:28 +0200

    Breakthrough - responsive images in 'tour-roll'

--- src/components/body_tour_roll.js
+++ src/components/body_tour_roll.js
@@ -6,47 +6,62 @@ class BodyTourRoll extends Component {
       <div className="container-fluid body-tour-roll">
         <div className="row">
           <div className="col-md-3">
-            <h2>Tour Dates</h2>
-            <h4>2016</h4>
+            <div className="roll-content lead-text">
+              <h2>Tour Dates</h2>
+              <h4>2016</h4>
+            </div>
           </div>
           <div className="col-md-3 pic">
-            <h2>January</h2>
+            <div>
+              <img src="/src/img/keiko1_cut.jpg" />
+            </div>
           </div>
           <div className="col-md-3">
-            <ul>
-              <li>UK, London, 12-01-2016</li>
-              <li>UK, Doublin, 14-01-2016</li>
-              <li>UK, Edinburgh, 15-01-2016</li>
-            </ul>
+            <div className="roll-content">
+              <h3>January</h3>
+              <ul>
+                <li>UK, London, 12-01-2016</li>
+                <li>UK, Doublin, 14-01-2016</li>
+                <li>UK, Edinburgh, 15-01-2016</li>
+              </ul>
+            </div>
           </div>
-          <div className="col-md-3">
-            <ul>
-              <li>France, Paris, 18-01-2016</li>
-              <li>France, Lyon, 19-01-2016</li>
-              <li>France, Nice, 21-01-2016</li>
-            </ul>
+          <div className="col-md-3 pic">
+            <div>
+              <img src="/src/img/keiko2_cut.jpg" />
+            </div>
           </div>
         </div>
         <div className="row">
           <div className="col-md-3 pic">
-            <h2>February</h2>
+            <div>
+              <img src="/src/img/keiko3_cut.jpg" />
+            </div>
           </div>
           <div className="col-md-3">
-            <ul>
-              <li>Spain, Barcelona, 18-02-2016</li>
-              <li>Spain, Madrid, 19-02-2016</li>
-              <li>Spain, Valencia, 21-02-2016</li>
-            </ul>
+            <div className="roll-content">
+              <h3>February</h3>
+              <ul>
+                <li>Spain, Barcelona, 18-02-2016</li>
+                <li>Spain, Madrid, 19-02-2016</li>
+                <li>Spain, Valencia, 21-02-2016</li>
+              </ul>
+            </div>
           </div>
           <div className="col-md-3 pic">
-            <h2>March</h2>
+            <div>
+              <img src="/src/img/keiko4_cut.jpg" />
+            </div>
           </div>
           <div className="col-md-3">
-            <ul>
-              <li>USA, New York, 01-03-2016</li>
-              <li>USA, San Francisco, 05-03-2016</li>
-              <li>USA, Los Angeles, 07-03-2016</li>
-            </ul>
+            <div className="roll-content">
+              <h3>March</h3>
+              <ul>
+                <li>USA, New York, 01-03-2016</li>
+                <li>USA, San Francisco, 05-03-2016</li>
+                <li>USA, Los Angeles, 07-03-2016</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
