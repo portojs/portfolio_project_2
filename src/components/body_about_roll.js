0b080ef9694ed78e02b822e3d39c956fc1683e1f
peter <peter.k.nets@gmail.com>
2016-03-21 10:42:08 +0200

    Fix 'body-about-roll' - DRY column rendering

--- src/components/body_about_roll.js
+++ src/components/body_about_roll.js
@@ -1,95 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BodyAboutRoll = () => {
-  return (
-    <div className="body-about-roll">
+const ArticleData1 = [
+  {
+    name: 'Los Angeles Outrider',
+    text: 'Consequat molestie parturient adipiscing ad scelerisque scelerisque tincidunt rutrum himenaeos hendrerit nunc vestibulum consectetur tempus ullamcorper tincidunt vestibulum vivamus.',
+    year: 1994
+  },
+  {
+    name: 'Vanity',
+    text: 'Consequat molestie parturient adipiscing ad scelerisque scelerisque tincidunt rutrum himenaeos hendrerit nunc vestibulum consectetur tempus ullamcorper tincidunt vestibulum vivamus.',
+    year: 1999
+  },
+  {
+    name: 'Vanity',
+    text: 'Consequat molestie parturient adipiscing ad scelerisque scelerisque tincidunt rutrum himenaeos hendrerit nunc vestibulum consectetur tempus ullamcorper tincidunt vestibulum vivamus.',
+    year: 2012
+  }
+];
 
-      <div className="row">
-        <div className="col-md-3 body-heading">
-          <h1 className="lead-text">Press</h1>
+const ArticleData2 = [
+  {
+    name: 'New York Times',
+    text: 'Consequat molestie parturient adipiscing ad scelerisque scelerisque tincidunt rutrum himenaeos hendrerit nunc vestibulum consectetur tempus ullamcorper tincidunt vestibulum vivamus.',
+    year: 2014
+  },
+  {
+    name: 'Esquire',
+    text: 'Consequat molestie parturient adipiscing ad scelerisque scelerisque tincidunt rutrum himenaeos hendrerit nunc vestibulum consectetur tempus ullamcorper tincidunt vestibulum vivamus.',
+    year: 2014
+  },
+  {
+    name: 'Empire',
+    text: 'Consequat molestie parturient adipiscing ad scelerisque scelerisque tincidunt rutrum himenaeos hendrerit nunc vestibulum consectetur tempus ullamcorper tincidunt vestibulum vivamus.',
+    year: 2016
+  }
+]
+
+class BodyAboutRoll extends Component {
+  renderArticles(articleData) {
+    return (
+      <div className="col-md-4">
+        <div className="body-about-article">
+          <h3>{articleData.name}</h3>
+          <p>{articleData.text}</p>
+          <h4 className="pull-right">{articleData.year}</h4>
         </div>
-        <div className="col-md-9">
-          <div className="row">
-            <div className="col-md-4">
-              <div className="body-about-article">
-                <h3>Los Angeles Outrider</h3>
-                <p>
-                  Consequat molestie parturient adipiscing ad scelerisque
-                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
-                  vestibulum consectetur tempus ullamcorper tincidunt
-                  vestibulum vivamus.
-                </p>
-                <h4 className="pull-right">1994</h4>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="body-about-article">
-                <h3>Wall Street Journal</h3>
-                <p>
-                  Consequat molestie parturient adipiscing ad scelerisque
-                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
-                  vestibulum consectetur tempus ullamcorper tincidunt
-                  vestibulum vivamus.
-                </p>
-                <h4 className="pull-right">1999</h4>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="body-about-article">
-                <h3>Vanity</h3>
-                <p>
-                  Consequat molestie parturient adipiscing ad scelerisque
-                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
-                  vestibulum consectetur tempus ullamcorper tincidunt
-                  vestibulum vivamus.
-                </p>
-                <h4 className="pull-right">2012</h4>
-              </div>
-            </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="body-about-roll">
+        <div className="row">
+          <div className="col-md-3 body-heading">
+            <h1 className="lead-text">Press</h1>
           </div>
-          <div className="row">
-            <div className="col-md-4">
-              <div className="body-about-article">
-                <h3>New York Times</h3>
-                <p>
-                  Consequat molestie parturient adipiscing ad scelerisque
-                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
-                  vestibulum consectetur tempus ullamcorper tincidunt
-                  vestibulum vivamus.
-                </p>
-                <h4 className="pull-right">2014</h4>
-              </div>
+          <div className="col-md-9">
+            <div className="row">
+              {ArticleData1.map(this.renderArticles)}
             </div>
-            <div className="col-md-4">
-              <div className="body-about-article">
-                <h3>Esquire</h3>
-                <p>
-                  Consequat molestie parturient adipiscing ad scelerisque
-                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
-                  vestibulum consectetur tempus ullamcorper tincidunt
-                  vestibulum vivamus.
-                </p>
-                <h4 className="pull-right">2014</h4>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="body-about-article">
-                <h3>Empire</h3>
-                <p>
-                  Consequat molestie parturient adipiscing ad scelerisque
-                  scelerisque tincidunt rutrum himenaeos hendrerit nunc
-                  vestibulum consectetur tempus ullamcorper tincidunt
-                  vestibulum vivamus.
-                </p>
-                <h4 className="pull-right">2016</h4>
-              </div>
+            <div className="row">
+              {ArticleData2.map(this.renderArticles)}
             </div>
           </div>
         </div>
       </div>
-
-    </div>
-  );
+    );
+  }
 }
 
 export default BodyAboutRoll;
