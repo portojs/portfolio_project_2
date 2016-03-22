29fd52601cb2e4fa6daa37c3cd1b726c327db4d5
peter <peter.k.nets@gmail.com>
2016-03-22 08:57:06 +0200

    Change roll-color to blue; start creating a body-discs component

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import ReactDOM from 'react-dom';
 
 import NavBar from './components/nav_bar';
 import BodyAbout from './components/body_about';
-import BodyAboutRoll from './components/body_about_roll';
-import BodyAboutRollAlt from './components/body_about_roll_alt';
 import BodyAboutRollAlt2 from './components/body_about_roll_alt2';
 import BodyTour from './components/body_tour';
 import BodyTourRoll from './components/body_tour_roll';
+import BodyDiscs from './components/body_discs';
 import Footer from './components/footer';
 
 class App extends Component {
@@ -21,6 +20,7 @@ class App extends Component {
         <BodyAboutRollAlt2 />
         <BodyTour />
         <BodyTourRoll />
+        <BodyDiscs />
         <Footer />
       </div>
     );
