5a7451a4307c47984e6e6aaaf36c4c5c60571e8c
peter <peter.k.nets@gmail.com>
2016-03-23 18:02:26 +0200

    Add image with inset shadow

--- src/components/body_discs_roll.js
+++ src/components/body_discs_roll.js
@@ -4,42 +4,8 @@ class BodyDiscsRoll extends Component {
   render() {
     return (
       <div className="container-fluid body-discs-roll">
-        <div className="row">
-          <div className="col-md-3">
-            <p>
-              Iaculis parturient ac ultricies nullam ipsum sit adipiscing
-               a ullamcorper nulla quam egestas a a ultrices parturient
-               leo elementum potenti nascetur pretium vestibulum nam
-               tortor ad condimentum gravida consectetur dapibus.
-            </p>
-          </div>
-          <div className="col-md-3">
-            <p>
-              A hac fermentum ultrices euismod sem sapien mi commodo
-              cubilia a adipiscing netus cubilia aenean a scelerisque
-              scelerisque consequat feugiat dui massa id taciti
-              nullam a volutpat dapibus.
-            </p>
-          </div>
-          <div className="col-md-3">
-            <p>
-              Iaculis parturient ac ultricies nullam ipsum sit
-              adipiscing a ullamcorper nulla quam egestas a
-              ultrices parturient leo elementum potenti nascetur
-              pretium vestibulum nam tortor ad condimentum gravida
-              consectetur dapibus.
-            </p>
-          </div>
-          <div className="col-md-3">
-            <p>
-              Iaculis parturient ac ultricies nullam ipsum sit
-              adipiscing a ullamcorper nulla quam egestas a a
-              ultrices parturient leo elementum potenti nascetur
-              pretium vestibulum nam tortor ad condimentum gravida
-              consectetur dapibus.
-            </p>
-          </div>
-        </div>
+        <img src="src/img/roll_1.jpg"/>
+        <div className="shadow"></div>
       </div>
     );
   }
