c289c8999892a68d391a30f93f28a753ce2c719b
peter <peter.k.nets@gmail.com>
2016-03-23 17:33:17 +0200

    Create temporary body-discs-roll component

---
+++ src/components/body_discs_roll.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class BodyDiscsRoll extends Component {
+  render() {
+    return (
+      <div className="container-fluid body-discs-roll">
+        <div className="row">
+          <div className="col-md-3">
+            <p>
+              Iaculis parturient ac ultricies nullam ipsum sit adipiscing
+               a ullamcorper nulla quam egestas a a ultrices parturient
+               leo elementum potenti nascetur pretium vestibulum nam
+               tortor ad condimentum gravida consectetur dapibus.
+            </p>
+          </div>
+          <div className="col-md-3">
+            <p>
+              A hac fermentum ultrices euismod sem sapien mi commodo
+              cubilia a adipiscing netus cubilia aenean a scelerisque
+              scelerisque consequat feugiat dui massa id taciti
+              nullam a volutpat dapibus.
+            </p>
+          </div>
+          <div className="col-md-3">
+            <p>
+              Iaculis parturient ac ultricies nullam ipsum sit
+              adipiscing a ullamcorper nulla quam egestas a
+              ultrices parturient leo elementum potenti nascetur
+              pretium vestibulum nam tortor ad condimentum gravida
+              consectetur dapibus.
+            </p>
+          </div>
+          <div className="col-md-3">
+            <p>
+              Iaculis parturient ac ultricies nullam ipsum sit
+              adipiscing a ullamcorper nulla quam egestas a a
+              ultrices parturient leo elementum potenti nascetur
+              pretium vestibulum nam tortor ad condimentum gravida
+              consectetur dapibus.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default BodyDiscsRoll;
